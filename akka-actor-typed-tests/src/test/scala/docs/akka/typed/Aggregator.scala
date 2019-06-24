56d6de80fc4984fecb8ce94f2d9d30826ebc0848
Patrik Nordwall <patrik.nordwall@gmail.com>
2019-06-24 15:33:59 +0200

    Examples of response aggregator and scatter gather

---
+++ akka-actor-typed-tests/src/test/scala/docs/akka/typed/Aggregator.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package docs.akka.typed
+
+import scala.concurrent.duration.FiniteDuration
+import scala.reflect.ClassTag
+
+import akka.actor.typed.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+
+object Aggregator {
+
+  sealed trait Command
+  private case object ReceiveTimeout extends Command
+  private case class WrappedReply[R](reply: R) extends Command
+
+  def apply[Reply: ClassTag, Aggregate](
+      sendRequests: ActorRef[Reply] => Unit,
+      expectedReplies: Int,
+      replyTo: ActorRef[Aggregate],
+      aggregateReplies: Set[Reply] => Aggregate,
+      timeout: FiniteDuration): Behavior[Command] = {
+    Behaviors.setup { context =>
+      context.setReceiveTimeout(timeout, ReceiveTimeout)
+      val replyAdapter = context.messageAdapter[Reply](WrappedReply(_))
+      sendRequests(replyAdapter)
+
+      def collecting(replies: Set[Reply]): Behavior[Command] = {
+        Behaviors.receiveMessage {
+          case WrappedReply(reply: Reply) =>
+            val newReplies = replies + reply
+            if (newReplies.size == expectedReplies) {
+              val result = aggregateReplies(replies)
+              replyTo ! result
+              Behaviors.stopped
+            } else
+              collecting(newReplies)
+
+          case ReceiveTimeout =>
+            val aggregate = aggregateReplies(replies)
+            replyTo ! aggregate
+            Behaviors.stopped
+        }
+      }
+
+      collecting(Set.empty)
+    }
+  }
+
+}
